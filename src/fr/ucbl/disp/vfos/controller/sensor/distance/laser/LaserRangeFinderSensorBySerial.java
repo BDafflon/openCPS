362eb8cef182e9a2505fa89aaeb9353561ebe8b4
pi <pi@raspberrypi>
2017-04-11 13:34:14 +0000

    lms

---
+++ src/fr/ucbl/disp/vfos/controller/sensor/distance/laser/LaserRangeFinderSensorBySerial.java
@@ -0,0 +1,20 @@
+package fr.ucbl.disp.vfos.controller.sensor.distance.laser;
+
+import fr.ucbl.disp.vfos.controller.data.AData;
+import fr.ucbl.disp.vfos.controller.sensor.distance.DistanceSensor;
+
+public class LaserRangeFinderSensorBySerial extends DistanceSensor {
+
+
+
+	public AData perceive() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
